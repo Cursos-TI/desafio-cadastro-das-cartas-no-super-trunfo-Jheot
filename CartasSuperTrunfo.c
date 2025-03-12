a0f87514a0f90d83eea48366e26df66521fce946
Jheot <josueinfoctt@gmail.com>
2025-03-12 02:28:55 +0000

    primeira commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
 
 int main (){
     char estado;
     int população;
     float 
     
-    printf("Desafio Super Trunfo! \n");
+    printf("Desafio Super Trunfo: \n");
 
     printf ("Carta 1");
     printf ("Nome da Cidade");
-    scanf("") // krl vou terminar depois essa porra   
+    scanf(""); // krl vou terminar depois essa porra   
 }
